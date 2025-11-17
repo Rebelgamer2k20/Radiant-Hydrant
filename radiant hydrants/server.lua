57834744549b1dc56d5236b91d6297704f9d8ce1
Rebelgamer2k20 <107582531+Rebelgamer2k20@users.noreply.github.com>
2025-11-17 03:32:54 -0500

    updated server lua

--- radiant hydrants/server.lua
+++ radiant hydrants/server.lua
@@ -10,51 +10,90 @@
 
 local oxmysql = exports.oxmysql
 
--- Load all hydrants on startup
+-----------------------------------------------------
+--  LOAD HYDRANTS ON SERVER START
+-----------------------------------------------------
 CreateThread(function()
     oxmysql:execute("SELECT * FROM radiant_hydrants", {}, function(result)
         for _, hydrant in ipairs(result) do
-            TriggerClientEvent("radiant_hydrants:spawnHydrant", -1, hydrant)
+            local data = {
+                id = hydrant.id,
+                x = hydrant.x,
+                y = hydrant.y,
+                z = hydrant.z,
+                heading = hydrant.heading
+            }
+
+            TriggerClientEvent("radiant_hydrants:spawnHydrant", -1, data)
         end
+
         print("^5[RADIANT DEV] ^7Loaded ^2" .. #result .. "^7 persistent hydrants.")
     end)
 end)
 
--- Command: place hydrant
+-----------------------------------------------------
+--  /hydrant place  |  /hydrant delete
+-----------------------------------------------------
 RegisterCommand("hydrant", function(source, args)
     local src = source
 
     if args[1] == "place" then
         TriggerClientEvent("radiant_hydrants:placeHydrant", src)
+
     elseif args[1] == "delete" then
         TriggerClientEvent("radiant_hydrants:deleteNearest", src)
+
     else
         TriggerClientEvent("chat:addMessage", src, {
-            color = {255, 0, 0},
-            multiline = true,
+            color = {255, 100, 0},
             args = {"Hydrants", "/hydrant place | /hydrant delete"}
         })
     end
 end)
 
--- Save hydrant to DB
+-----------------------------------------------------
+--  SAVE HYDRANT TO DATABASE
+-----------------------------------------------------
 RegisterNetEvent("radiant_hydrants:saveHydrant")
 AddEventHandler("radiant_hydrants:saveHydrant", function(coords, heading)
+
     oxmysql:insert(
         "INSERT INTO radiant_hydrants (x, y, z, heading) VALUES (?, ?, ?, ?)",
         {coords.x, coords.y, coords.z, heading},
+
         function(id)
-            coords.id = id
-            TriggerClientEvent("radiant_hydrants:spawnHydrant", -1, coords)
-            print("^5[RADIANT DEV] ^7Hydrant placed & saved with ID: ^2" .. id)
+            if not id then
+                print("^1[RADIANT DEV ERROR] Could not insert hydrant into DB.^7")
+                return
+            end
+
+            local data = {
+                id = id,
+                x = coords.x,
+                y = coords.y,
+                z = coords.z,
+                heading = heading
+            }
+
+            TriggerClientEvent("radiant_hydrants:spawnHydrant", -1, data)
+
+            print("^5[RADIANT DEV] ^7Hydrant saved & spawned with ID: ^2" .. id)
         end
     )
 end)
 
--- Remove hydrant from DB
+-----------------------------------------------------
+--  DELETE HYDRANT FROM DATABASE
+-----------------------------------------------------
 RegisterNetEvent("radiant_hydrants:deleteHydrant")
 AddEventHandler("radiant_hydrants:deleteHydrant", function(id)
-    oxmysql:execute("DELETE FROM radiant_hydrants WHERE id = ?", {id})
-    TriggerClientEvent("radiant_hydrants:removeHydrant", -1, id)
-    print("^1[RADIANT DEV] Hydrant deleted ID: " .. id)
+
+    oxmysql:execute("DELETE FROM radiant_hydrants WHERE id = ?", {id}, function(affected)
+        if affected and affected > 0 then
+            TriggerClientEvent("radiant_hydrants:removeHydrant", -1, id)
+            print("^1[RADIANT DEV] Hydrant deleted ID: " .. id)
+        else
+            print("^1[RADIANT DEV] ERROR: ID not found / no rows removed.^7")
+        end
+    end)
 end)
